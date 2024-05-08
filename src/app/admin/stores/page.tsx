750e10b5f4c60eb35b1598ae63fb60da7c35e43a
todddenmar <todddenmar@gmail.com>
2024-05-08 10:12:13 +0800

    added sanity root to admin stores page

--- src/app/admin/stores/page.tsx
+++ src/app/admin/stores/page.tsx
@@ -1,7 +1,19 @@
 import StoresTable from '@/components/admin/tables/StoreTables';
+import { client, urlFor } from '@/lib/client';
+import { getSanityRootData } from '@/lib/queries/sanity';
 import React from 'react';
 
-function AdminStoresPage() {
+async function AdminStoresPage() {
+  const data = await getSanityRootData({ slug: 'logos', client: client });
+  console.log({ data });
+  let images: string[] = [];
+  data.forEach((item: any) => {
+    return item.images.forEach((image: any) => {
+      const src = urlFor(image).url();
+      images.push(src);
+    });
+  });
+  console.log(images);
   return (
     <section className="mt-2 md:mt-5 flex flex-col">
       <StoresTable />
--- src/lib/queries/sanity.ts
+++ src/lib/queries/sanity.ts
@@ -9,3 +9,16 @@ export async function getStoreSanityData({
   const data = await client.fetch(query);
   return data;
 }
+
+export async function getSanityRootData({
+  slug,
+  client,
+}: {
+  slug: string;
+  client: any;
+}) {
+  const query = `*[_type == "root"]{...,"slug":${slug}}`;
+  const data = await client.fetch(query);
+  return data;
+}
+
